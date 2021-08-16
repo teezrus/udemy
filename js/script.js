9f4c31548d1a9fe51762572642bc8d64194ed9ac
andrew <teezkit@ya.ru>
2021-08-16 19:08:50 +0300

    first progect

---
+++ js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// alert("dfwfe")
+// prompt('dsdsd', '')
+// confirm('css')
+
+// const answer = [];
+// answer[0] = prompt('name', '');
+// answer[1] = prompt('lastname', '');
+// answer[2] = +prompt('age', '');
+// delete answer[2];
+// console.log(answer)
+
+const category = 'toys';
+
+console.log(`http://rename/${category}`)
+
+alert(`Good moning, ${category}`);
